98d074fb67f494ced52e4057d889536d2b6144da
jnewman <jonathan_newman@legalaccessplans.com>
2015-11-30 08:09:47 -0600

    can now create todo entries

--- scripts/viewModels/TodoItem.js
+++ scripts/viewModels/TodoItem.js
@@ -1,11 +1,7 @@
-define(['footwork'],
-  function(fw) {
-    return fw.viewModel.create({
-      namespace: 'TodoItem',
-      initialize: function(thingToDo) {
-        // store the string text entered by user as thingToDo
-        this.thingToDo = fw.observable(thingToDo);
-      }
-    });
+var TodoItem = fw.viewModel.create({
+  namespace: 'TodoItem',
+  initialize: function(thingToDo) {
+    // store the string text entered by user as thingToDo
+    this.thingToDo = fw.observable(thingToDo);
   }
-);
+});
