cc7671de0d4df92f141cf2714eb436fc1001dad3
jnewman <jonathan_newman@legalaccessplans.com>
2015-11-30 08:09:46 -0600

    now displaying username in header

--- scripts/viewModels/TodoItem.js
+++ scripts/viewModels/TodoItem.js
@@ -3,25 +3,8 @@ define(['footwork'],
     return fw.viewModel.create({
       namespace: 'TodoItem',
       initialize: function(thingToDo) {
-        // create the namespace 'channel' we will use to talk to the TodoList
-        this.todoListNamespace = fw.namespace('TodoList');
-
         // store the string text entered by user as thingToDo
         this.thingToDo = fw.observable(thingToDo);
-
-        // store the state of the TodoItem (true = done, false = not done)
-        this.isDone = fw.observable(false);
-
-        // toggle the state of this TodoItem
-        this.toggleDone = function() {
-          this.isDone(!this.isDone());
-        };
-
-        // method used to send the deleteItem command when a user clicks the 'X'
-        this.deleteItem = function() {
-          // tell the TodoList to delete this item
-          this.todoListNamespace.command('deleteItem', this);
-        };
       }
     });
   }
