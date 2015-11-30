d870a4a69b5d7d3e04f7876e8fe6809569c52ed0
jnewman <jonathan_newman@legalaccessplans.com>
2015-11-30 08:09:41 -0600

    now rendering the number of remaining todo entries in the footer

--- scripts/viewModels/Footer.js
+++ scripts/viewModels/Footer.js
@@ -3,31 +3,8 @@ define(['footwork'],
     return fw.viewModel.create({
       namespace: 'Footer',
       initialize: function() {
-        // create the namespace 'channel' we will use to talk to the TodoList
-        this.todoListNamespace = fw.namespace('TodoList');
-
-        // numRemainingTodos will receive its value from the TodoList
+        // numRemainingTodo will receive its value from the TodoList
         this.numRemainingTodos = fw.observable().receiveFrom('TodoList', 'numRemainingTodos');
-
-        // method which sends the command to clear all completed todos
-        this.clearCompleted = function() {
-          // tell the TodoList to clear the completed todo entries
-          this.todoListNamespace.command('clearCompleted');
-        };
-
-        // loggedInUser will receive its value from the MainRouter
-        this.loggedInUser = fw.observable().receiveFrom('MainRouter', 'loggedInUser');
-
-        // the callbacks which return the needed filter routes
-        this.allRoute = function() {
-          return '/todo/' + this.loggedInUser() + '/all';
-        }.bind(this);
-        this.activeRoute = function() {
-          return '/todo/' + this.loggedInUser() + '/active';
-        }.bind(this);
-        this.completedRoute = function() {
-          return '/todo/' + this.loggedInUser() + '/completed';
-        }.bind(this);
       }
     });
   }
