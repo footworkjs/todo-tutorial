6ea133e36e6caefa7d96b6176309a66821a511d1
jnewman <jonathan_newman@legalaccessplans.com>
2015-11-30 08:09:27 -0600

    added base MainRouter and 2 route definitions

--- scripts/MainRouter.js
+++ scripts/MainRouter.js
@@ -2,57 +2,19 @@ define(['footwork'],
   function(fw) {
     return fw.router.create({
       namespace: 'MainRouter',
-
-      /**
-       * Add the initialize method which is called when the MainRouter is instantiated.
-       * We then setup the event handler for 'userLogin' messages received on this.$namespace
-       */
-      initialize: function() {
-        // Create reference to the router we will use in the callback
-        var router = this;
-
-        /**
-         * broadcast the username of the current user as 'loggedInUser'
-         * (the default value is null indicating 'logged out')
-         */
-        this.loggedInUser = fw.observable(null).broadcastAs('loggedInUser');
-
-        // broadcast the state we wish to filter the list for
-        this.listFilter = fw.observable(null).broadcastAs('listFilter');
-
-        // Create the event handler which receives the 'userLogin' event (and corresponding userName)
-        this.$namespace.event.handler('userLogin', function(username) {
-          // Tell the router to route to '/todo/username' if we have a username
-          if(username) {
-            router.setState('/todo/' + username + '/all');
-          }
-        });
-      },
-
       routes: [
         {
           route: '/',
           title: 'Todo Application Tutorial',
           controller: function() {
-            // the user is logged out if at the default route, lets nullify the username
-            this.loggedInUser(null);
-
             // show our login page here
-            this.outlet('mainView', 'login-page');
           }
         },
         {
-          route: '/todo/:username(/:state)', // required parameter 'username', optional parameter 'state'
+          route: '/todo',
           title: 'Todo List',
-          controller: function(username, state) {
-            // set the loggedInUser value to the username
-            this.loggedInUser(username);
-
-            // set the listFilter value to the supplied state (will be broadcast to the TodoList)
-            this.listFilter(state || 'all');
-
+          controller: function() {
             // show our todo list page here
-            this.outlet('mainView', 'todo-page');
           }
         }
       ]
--- scripts/app.js
+++ scripts/app.js
@@ -12,16 +12,8 @@ requirejs.config({
 
 require(['footwork'],
   function(fw) {
-    /**
-     * Tell footwork it can find the 'LoginForm', 'TodoList', 'Header' and
-     * 'Footer' viewModels inside of scripts/viewModels/
-     */
-    fw.viewModel.registerLocation([
-      'LoginForm', // will load from scripts/viewModels/LoginForm.js
-      'TodoList', // will load from scripts/viewModels/TodoList.js
-      'Header', // will load from scripts/viewModels/Header.js
-      'Footer' // will load from scripts/viewModels/Footer.js
-    ], 'scripts/viewModels/');
+    // Tell footwork where it can find the TodoList viewModel.
+    fw.viewModel.registerLocation('TodoList', 'scripts/viewModels/');
 
     /**
      * Here we provide the entire filename to footwork because the lowercase
@@ -31,18 +23,6 @@ require(['footwork'],
       viewModel: 'scripts/viewModels/TodoForm', // loads scripts/viewModels/TodoForm.js
       template: 'scripts/templates/TodoForm' // loads scripts/templates/TodoForm.html
     });
-
-    /**
-     * Register the location of the 'MainRouter'
-     * Loads from scripts/MainRouter.js
-     */
-    fw.router.registerLocation('MainRouter', 'scripts/');
-
-    // Tell footwork it can find the login-page and todo-page inside of scripts/views/
-    fw.outlets.registerViewLocation([
-      'login-page', // will load from scripts/views/login-page.html
-      'todo-page' // will load from scripts/views/todo-page.html
-    ], 'scripts/views/');
 
     // tell footwork to startup and begin binding
     fw.start();
